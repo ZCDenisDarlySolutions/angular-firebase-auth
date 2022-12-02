be25af092b6d13ce84d3123f51c2d08c39aa7795
ZCDenisDarlySolutions <denis.zc@darly.solutions>
2022-12-02 18:59:56 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/content/content.module').then(
+        (module) => module.ContentModule
+      ),
+  },
+  {
+    path: 'log-in',
+    loadChildren: () =>
+      import('./pages/log-in/log-in.module').then(
+        (module) => module.LogInModule
+      ),
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () =>
+      import('./pages/sign-up/sign-up.module').then(
+        (module) => module.SignUpModule
+      ),
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () =>
+      import('./pages/forgot-password/forgot-password.module').then(
+        (module) => module.ForgotPasswordModule
+      ),
+  },
+  { path: '**', redirectTo: '/login' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
