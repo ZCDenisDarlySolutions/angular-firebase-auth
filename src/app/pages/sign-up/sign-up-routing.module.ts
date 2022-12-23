1aa0bb261ecdfb551e018c2c21efab7c629142a2
ZCDenisDarlySolutions <119358804+ZCDenisDarlySolutions@users.noreply.github.com>
2022-12-23 17:22:46 +0200

    fix: Load information about sign-up step

--- src/app/pages/sign-up/sign-up-routing.module.ts
+++ src/app/pages/sign-up/sign-up-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpFirstStepComponent } from './shared/components/sign-up-first-step/sign-up-first-step.component';
 import { SignUpSecondStepComponent } from './shared/components/sign-up-second-step/sign-up-second-step.component';
 import { SignUpComponent } from './sign-up.component';
+import { SIGN_UP_ROUTE } from './sign-up.constants';
 
 const routes: Routes = [
   {
     path: '',
     component: SignUpComponent,
     children: [
-      { path: '', redirectTo: 'first-step', pathMatch: 'full' },
-      { path: 'first-step', component: SignUpFirstStepComponent },
-      { path: 'second-step', component: SignUpSecondStepComponent },
+      { path: '', redirectTo: SIGN_UP_ROUTE.firstStep, pathMatch: 'full' },
+      { path: SIGN_UP_ROUTE.firstStep, component: SignUpFirstStepComponent },
+      { path: SIGN_UP_ROUTE.secondStep, component: SignUpSecondStepComponent },
     ],
   },
 ];
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SIGN_UP_ROUTE } from './sign-up.constants';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,7 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  get step() {
-    return 1;
+  public step: number = 1;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.step = this.router.url.endsWith(SIGN_UP_ROUTE.firstStep) ? 1 : 2;
   }
 }
