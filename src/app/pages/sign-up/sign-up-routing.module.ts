d47a78290ae3ba7a40742f936f7d86c06d22f3cd
ZCDenisDarlySolutions <119358804+ZCDenisDarlySolutions@users.noreply.github.com>
2022-12-23 16:56:12 +0200

    fix: Sign up page logic

--- src/app/pages/sign-up/sign-up-routing.module.ts
+++ src/app/pages/sign-up/sign-up-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { SignUpFirstStepComponent } from './shared/components/sign-up-first-step/sign-up-first-step.component';
+import { SignUpSecondStepComponent } from './shared/components/sign-up-second-step/sign-up-second-step.component';
 import { SignUpComponent } from './sign-up.component';
 
-const routes: Routes = [{ path: '', component: SignUpComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: SignUpComponent,
+    children: [
+      { path: '', redirectTo: 'first-step', pathMatch: 'full' },
+      { path: 'first-step', component: SignUpFirstStepComponent },
+      { path: 'second-step', component: SignUpSecondStepComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
-  step = 1;
+  get step() {
+    return 1;
+  }
 }
